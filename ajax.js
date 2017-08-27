860bd4c6afe326dd14b4045bc99773682998a49c
N8Sayer <forcelord50@gmail.com>
2017-08-27 15:52:34 -0500

    changes

--- ajax.js
+++ ajax.js
@@ -1,15 +1,30 @@
 var url = 'https://papasandbeer.agilecrm.com/dev/api/contacts/5690964005879808';
-url += '?' + $.param({
-});
+// url += '?' + $.param({
+// });
 
-$.ajax ({
-  url: url,
-  method: 'GET',
-  beforeSend: function (xhr) {
-    xhr.setRequestHeader ("Authorization", "Basic " + btoa('papas@zitrocreative.com' + ":" + '1o1v57vguh2naqkh7fkd2h0fis'));
+var username = 'papas@zitrocreative.com',
+    password = '1o1v57vguh2naqkh7fkd2h0fis';
+
+function make_base_auth(user, password) {
+  var tok = user + ':' + password;
+  var hash = btoa(tok);
+  return "Basic " + hash;
+}
+var auth = make_base_auth(username,password);
+
+var settings = {
+  "async": true,
+  "crossDomain": true,
+  "url": url,
+  "method": "GET",
+  "headers": {
+    "authorization": auth,
+    "cache-control": "no-cache"
   }
-}).done(function(result) {
+}
+
+$.ajax(settings).done(function (response) {
   $('body').append(result);
-}).fail(function(result) {
-  $('body').append('Failed');
+}).fail(function (response) {
+  $('body').append("Failed");
 });
